64f3d87bdc742d6837c46ad67aa2935c09ae6391
Anna-Lazakovich <anna27659@gmail.com>
2025-05-06 22:32:52 +0300

    style: Styling the 404 page – missing.module.css

--- src/component/missing/Missing.jsx
+++ src/component/missing/Missing.jsx
@@ -1,7 +1,17 @@
+import donut from "../../img/donut.jpg";
+import style from './Missing.module.css';
+
 function Missing() {
   return (
-    <main>
-      <h1>404 not found</h1>
+    <main className={style.missingPage}>
+      <h1 className={style.h1}>ошибка</h1>
+      <div className={style.missing}>
+        4
+        <img src={donut} className={style.donut} alt='Ошибка 404'></img>
+        4
+      </div>
+        <p className={style.missingText}>Ой! Кажется что-то пошло не так. Страница, которую вы пытаетесь загрузить, не существует.
+        </p>
     </main>
   )
 }
